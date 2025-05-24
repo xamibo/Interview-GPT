08d7304542aaee014b12c4f7a74ad5620882386d
xamibo <xamibose2001@gmail.com>
2025-05-24 15:15:39 +0530

    some changes

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ import { dark } from "@clerk/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AI Career Coach",
+  title: "InterviewGPT - Your AI Career Coach",
   description: "",
 };
 
